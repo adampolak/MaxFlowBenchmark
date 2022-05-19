e1bd6e9c3a804711816deffd639e9f238a43d8a6
Maxim Zub <maxim.zub@latoken.com>
2022-05-19 04:05:56 +0200

    graph preprocessing

---
+++ data/preprocess_graph.py
@@ -0,0 +1,39 @@
+with open('bone.n6c10/bone_subxyz_subxy.n6c10.max') as f:
+    dct = {}
+    S = 0
+    T = 0
+    v_num = 0
+    for line in f:
+        if line[0] == 'p':
+            p, mx, vn, en = [x for x in line.split()]
+            v_num = int(vn)
+            continue
+        if line[0] == 'n':
+            n, u, t = [x for x in line.split()]
+            u = int(u)
+            if t == 's':
+                S = u
+            else:
+                T = u
+            continue
+        elif line[0] != 'a':
+            continue
+        a, u, v, cur_f = [x for x in line.split()]
+
+        u = int(u)
+        v = int(v)
+        cur_f = int(cur_f)
+        if cur_f == 0:
+            continue
+
+        curvalue = 0
+        try:
+            curvalue = dct[(u, v)]
+        except:
+            curvalue = 0
+        dct[(u, v)] = curvalue + cur_f
+    print('p max ' + str(v_num) + ' ' + str(len(dct)))
+    print('n ' + str(S) + ' s')
+    print('n ' + str(T) + ' t')
+    for key, value in dct.items():
+        print('a ' + str(key[0]) + ' ' + str(key[1]) + ' ' + str(value))
